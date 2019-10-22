7d6407590e9c7ac676bd105663d3437b186317db
Maksim Kildishev <thanatormax@gmail.com>
2019-10-23 00:44:44 +0300

    Code added

---
+++ src/lab1_keras.py
@@ -0,0 +1,43 @@
+from keras import models
+from keras.datasets import mnist
+from keras import layers
+from keras.utils import to_categorical
+from keras import optimizers
+from datetime import datetime
+
+
+def run(hidden_size=512, batch_size=128, rate=0.001, epochs=5):
+    (train_images, train_labels), (test_images, test_labels) = mnist.load_data()
+    train_images = train_images.reshape((60000, 28 * 28))
+    train_images = train_images.astype('float32') / 255
+    test_images = test_images.reshape((10000, 28 * 28))
+    test_images = test_images.astype('float32') / 255
+
+    train_labels = to_categorical(train_labels)
+    test_labels = to_categorical(test_labels)
+
+    network = models.Sequential()
+    network.add(layers.Dense(hidden_size, activation='relu', input_shape=(28 * 28,)))
+    network.add(layers.Dense(10, activation='softmax'))
+    rmsprop = optimizers.rmsprop(rate)
+    network.compile(optimizer=rmsprop,
+                    loss='categorical_crossentropy',
+                    metrics=['accuracy'])
+    time_start = datetime.now()
+    network.fit(train_images, train_labels, epochs=epochs, batch_size=batch_size)
+    time = datetime.now() - time_start
+    train_result = network.evaluate(train_images, train_labels)
+    test_result = network.evaluate(test_images, test_labels)
+    return train_result, test_result, time
+
+
+if __name__ == '__main__':
+    train_result, test_result, time = run()
+    print('Train loss: ', train_result[0])
+    print('Train accuracy: ', train_result[1])
+    print('Test loss: ', test_result[0])
+    print('Test accuracy: ', test_result[1])
+    print('Time: ', time)
+
+
+
---
+++ src/main.py
@@ -0,0 +1,36 @@
+from argparse import ArgumentParser
+import lab1_impl
+import lab1_keras
+
+
+parser = ArgumentParser()
+parser.add_argument('--type', type=str, choices=['lab', 'keras'], default='lab',
+                    help='Choosing network for run')
+parser.add_argument('--hidden_num', type=int,  default=240,
+                    help='Hidden layer nodes num')
+parser.add_argument('--batch_size', type=int, default=128,
+                    help='Sample batch size')
+parser.add_argument('--rate', type=float, default=0.1,
+                    help='Learning rate')
+parser.add_argument('--epochs', type=int, default=5,
+                    help='Epochs count')
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    if args.type == 'lab':
+        train_result, test_result, time = lab1_impl.run(args.hidden_num, args.batch_size, args.rate, args.epochs)
+        print('Train loss: ', train_result[0])
+        print('Train accuracy: ', train_result[1])
+        print('Test loss: ', test_result[0])
+        print('Test accuracy: ', test_result[1])
+        print('Time: ', time)
+    else:
+        train_result, test_result, time = lab1_keras.run(args.hidden_num, args.batch_size, args.rate, args.epochs)
+        print('Train loss: ', train_result[0])
+        print('Train accuracy: ', train_result[1])
+        print('Test loss: ', test_result[0])
+        print('Test accuracy: ', test_result[1])
+        print('Time: ', time)
+
+
+
